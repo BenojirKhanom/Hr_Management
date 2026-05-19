7b58ca0a1d02348de4d33514c3f3736a575187d6
HR_System <bithishanaj14@gmail.com>
2026-05-19 23:48:51 +0600

    add

--- HR_Management/Models/Employee_Ditailse.cs
+++ HR_Management/Models/Employee_Ditailse.cs
@@ -2,5 +2,19 @@
 {
     public class Employee_Ditailse
     {
+        public int Id { get; set; }
+
+        public DateTime JoiningDate { get; set; }
+
+        public bool ActivityStatus { get; set; }
+
+        public string Department { get; set; }
+
+        public string JoinDesignation { get; set; }
+
+        public string EmployeeStatus { get; set; }
+
+        public string BranchName { get; set; }
+
     }
 }
