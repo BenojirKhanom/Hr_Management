14c5853fb868ee7fedbed7a1d0410dc4ab4d8a81
HR_System <bithishanaj14@gmail.com>
2026-05-20 12:52:31 +0600

    add

--- HR_Management/Controllers/EmployeesController.cs
+++ HR_Management/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using HR_Management.Data;
+using HR_Management.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HR_Management.Controllers
@@ -10,9 +11,24 @@ namespace HR_Management.Controllers
         {
             _Context = Context;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee_Ditailse employee_Ditailse)
+        {
+            if (ModelState.IsValid)
+            {
+                _Context.Employees.Add(employee_Ditailse);
+
+                _Context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(employee_Ditailse);
         }
     }
 }
