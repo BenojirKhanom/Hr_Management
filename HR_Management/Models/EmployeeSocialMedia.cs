164accda197da26be6b0a1488d5f247523445bb0
HR_System <bithishanaj14@gmail.com>
2026-05-20 08:39:06 +0600

    add

--- HR_Management/Models/EmployeeSocialMedia.cs
+++ HR_Management/Models/EmployeeSocialMedia.cs
@@ -1,6 +1,17 @@
-﻿namespace HR_Management.Models
+﻿using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HR_Management.Models
 {
     public class EmployeeSocialMedia
     {
+        public int Id { get; set; }
+        public string LinkedInProfileURL { get; set; }
+        public string FacebookProfileURL { get; set; }
+        public string TwitterHandleURL { get; set; }
+        public string InstagramHandleURL { get; set; }
+        public string GitHubProfileURL { get; set; }
+        public string PortfolioURL { get; set; }
+
     }
 }
