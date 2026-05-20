cdd6927df163b5518484242663050a49d52b0b06
HR_System <bithishanaj14@gmail.com>
2026-05-20 11:48:42 +0600

    add

--- HR_Management/Controllers/EmployeesController.cs
+++ HR_Management/Controllers/EmployeesController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HR_Management.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HR_Management.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly AppDbContext _Context;
+        public EmployeesController(AppDbContext Context)
+        {
+            _Context = Context;
+        }
         public IActionResult Index()
         {
             return View();
