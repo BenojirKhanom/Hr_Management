e48fba6c2289165e9b5014af4827f99d1082bab8
HR_System <bithishanaj14@gmail.com>
2026-05-21 23:15:44 +0600

    add

--- HR_Management/Controllers/EmployeesController.cs
+++ HR_Management/Controllers/EmployeesController.cs
@@ -23,8 +23,29 @@ namespace HR_Management.Controllers
         {
             if (ModelState.IsValid)
             {
-                _Context.EmployeeDitailse.Add(model.EmployeeDetails);
-                _Context.EmployeeSocialMedia.Add(model.EmployeeSocialMedia);
+                var details = new EmployeeDitailse
+                {
+                    JoiningDate = model.EmployeeDetails.JoiningDate,
+                    ActivityStatus = model.EmployeeDetails.ActivityStatus,
+                    Department = model.EmployeeDetails.Department,
+                    JoinDesignation = model.EmployeeDetails.JoinDesignation,
+                    EmployeeStatus = model.EmployeeDetails.EmployeeStatus,
+                    BranchName = model.EmployeeDetails.BranchName
+                };
+
+                var social = new EmployeeSocialMedia
+                {
+                    LinkedInProfileURL = model.EmployeeSocialMedia.LinkedInProfileURL,
+                    FacebookProfileURL = model.EmployeeSocialMedia.FacebookProfileURL,
+                    TwitterHandleURL = model.EmployeeSocialMedia.TwitterHandleURL,
+                    InstagramHandleURL = model.EmployeeSocialMedia.InstagramHandleURL,
+                    GitHubProfileURL = model.EmployeeSocialMedia.GitHubProfileURL,
+                    PortfolioURL = model.EmployeeSocialMedia.PortfolioURL
+                };
+
+                _Context.EmployeeDitailse.Add(details);
+
+                _Context.EmployeeSocialMedia.Add(social);
 
                 _Context.SaveChanges();
 
@@ -33,6 +54,8 @@ namespace HR_Management.Controllers
 
             return View(model);
         }
+
+
     }
     }
 
--- HR_Management/obj/Debug/net10.0/HR_Management.AssemblyInfo.cs
+++ HR_Management/obj/Debug/net10.0/HR_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d06203ec98a6241b4f0c75e88ec5ee130a4b714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ca1e3d4da5482d4d55ea3981ab4c0a3603c8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
