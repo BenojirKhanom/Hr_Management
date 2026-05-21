5d06203ec98a6241b4f0c75e88ec5ee130a4b714
HR_System <bithishanaj14@gmail.com>
2026-05-21 12:43:57 +0600

    add

--- HR_Management/obj/Debug/net10.0/HR_Management.AssemblyInfo.cs
+++ HR_Management/obj/Debug/net10.0/HR_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0458b1836da09b83df7712ad8e2cc1120a45138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0243330e2167a639b0dbc9e673ea7856131150a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
