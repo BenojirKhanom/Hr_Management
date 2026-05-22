0b02a70a15c161e3661ca56c4ef51c7fc8dd1080
HR_System <bithishanaj14@gmail.com>
2026-05-22 14:19:13 +0600

    add

--- HR_Management/Controllers/EmployeesController.cs
+++ HR_Management/Controllers/EmployeesController.cs
@@ -8,17 +8,21 @@ namespace HR_Management.Controllers
     public class EmployeesController : Controller
     {
         private readonly AppDbContext _Context;
+
         public EmployeesController(AppDbContext Context)
         {
             _Context = Context;
         }
+
+        [HttpGet]
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Create(EmployeeViewModel model)
+        public IActionResult Index(EmployeeViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -27,28 +31,22 @@ namespace HR_Management.Controllers
                     JoiningDate = model.EmployeeDetails.JoiningDate,
                     Department = model.EmployeeDetails.Department,
                     JoinDesignation = model.EmployeeDetails.JoinDesignation,
-               
                 };
 
                 var social = new EmployeeSocialMedia
                 {
                     GitHubProfileURL = model.EmployeeSocialMedia.GitHubProfileURL,
-                    
                 };
 
                 _Context.EmployeeDitailse.Add(details);
-
                 _Context.EmployeeSocialMedia.Add(social);
-
                 _Context.SaveChanges();
 
-                return RedirectToAction("Create");
+                return RedirectToAction("Index");
             }
 
             return View(model);
         }
-
-
     }
-    }
+}
 
--- HR_Management/obj/Debug/net10.0/HR_Management.AssemblyInfo.cs
+++ HR_Management/obj/Debug/net10.0/HR_Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f18a9b8d57aeaa7e08a9dd95a0e919aa0620dac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4558ac151c663a118f468752ecd20b63228e8aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
